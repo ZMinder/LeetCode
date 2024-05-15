f756fee120de4caeeb69e3f1895eea81915decf1
CMinder <minderctrl@gmail.com>
2024-05-15 17:06:50 +0800

    P2589 按照截止时间升序排序，保证后续任务与当前任务要么没有交集，要么存在后缀，因此，当前任务要尽可能晚的完成，可以保证最大化利用时间

--- src/P2501_2600/P2581_2590/P2589.java
+++ src/P2501_2600/P2581_2590/P2589.java
@@ -2,7 +2,8 @@ package P2501_2600.P2581_2590;
 
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * 你有一台电脑，它可以 同时 运行无数个任务。给你一个二维整数数组 tasks ，其中 tasks[i] = [starti, endi, durationi] 表示第 i 个任务需要在 闭区间 时间段 [starti, endi] 内运行 durationi 个整数时间点（但不需要连续）。
@@ -12,70 +13,44 @@ import java.util.*;
  * 请你返回完成所有任务的情况下，电脑最少需要运行多少秒。
  */
 public class P2589 {
-    public class Task {
-        public int start;//允许运行的起始时间
-        public int end;//允许运行的结束时间
-        public int duration;//运行时间
-
-        public Task(int start, int end, int duration) {
-            this.start = start;
-            this.end = end;
-            this.duration = duration;
+    public class MyComparator implements Comparator<int[]> {
+        @Override
+        public int compare(int[] o1, int[] o2) {
+            //以截止时间升序排序 保证了后续任务要么跟当前任务没有交集，要么存在相同的后缀，
+            // 当前任务尽可能在较晚的时间段完成，后续的任务可以借前面的任务运行时间运行
+            return o1[1] - o2[1];
         }
     }
 
     public int findMinimumTime(int[][] tasks) {
-        HashMap<Integer, HashSet<Task>> timeMap = new HashMap<>();//记录每个时间段中可以运行的任务
+        Arrays.sort(tasks, new MyComparator());
+        boolean[] run = new boolean[2005];//记录每个时间点是否存在可运行的任务
+        int res = 0;//最终所需要的运行时间
         for (int i = 0; i < tasks.length; i++) {
-            Task task = new Task(tasks[i][0], tasks[i][1], tasks[i][2]);
-            for (int j = tasks[i][0]; j <= tasks[i][1]; j++) {
-                HashSet<Task> set = timeMap.getOrDefault(j, new HashSet<>());//该时间段可以运行的任务
-                set.add(task);//加入当前任务
-                timeMap.put(j, set);//更新map
-            }
-        }
-        int res = 0;//记录运行时间
-        while (!timeMap.isEmpty()) {
-            int time = findMax(timeMap);
-            HashSet<Task> task = timeMap.get(time);//当前时间段的任务
-            if (task.isEmpty()) {//所有任务均已完成
-                break;
-            }
-            ArrayList<Task> zero = new ArrayList<>();//存储已经完成的任务
-            for (Task t : task) {//当前任务运行时间减1
-                t.duration--;
-                if (t.duration == 0) {//当前任务执行结束
-                    zero.add(t);//加入zero
+            int start = tasks[i][0];//当前任务的起始运行时间
+            int end = tasks[i][1];//当前任务的结束运行时间
+            int required = tasks[i][2];//当前任务需要的运行时间
+            for (int j = start; j <= end; j++) {
+                if (run[j]) {//如果当前时间段之前存在任务运行 可以顺带帮我运行1个时间段
+                    required--;
                 }
             }
-            for (Task t : zero) {
-                for (int i = t.start; i <= t.end; i++) {//当前任务起始时间到结束时间移出当前任务
-                    if (timeMap.get(i) != null) {//当前时间未被使用过
-                        timeMap.get(i).remove(t);
-                    }
+            for (int j = end; required > 0; j--) {//剩余运行时间在尽可能晚的时间点完成
+                if (!run[j]) {
+                    //可能存在当前任务1-6 前面的任务是3-4,5-5,我需要时间5
+                    // 因此时间2也是需要的 并且在3-5required不能--
+                    run[j] = true;
+                    required--;
+                    res++;
                 }
             }
-            timeMap.remove(time);//移出当前时间
-            res++;//如果有任务被完成 运行时间加1
         }
         return res;
     }
 
-    public int findMax(HashMap<Integer, HashSet<Task>> timeMap) {
-        int time = 0;//记录同时运行任务最多的时间段
-        int tasks = -1;//记录任务数
-        for (Map.Entry<Integer, HashSet<Task>> entry : timeMap.entrySet()) {
-            if (entry.getValue().size() > tasks) {//选取任务数量最多的时间段
-                tasks = entry.getValue().size();
-                time = entry.getKey();
-            }
-        }
-        return time;
-    }
-
     @Test
     public void testConvert() {
-        String str = "[[68,129,2],[58,86,9],[112,142,10],[106,108,1],[48,48,1],[116,143,2],[28,43,5],[1,1,1],[75,83,3],[104,136,10],[11,11,1],[60,63,1],[73,111,8],[57,57,1],[117,119,3],[25,38,2],[20,21,1],[78,80,2],[17,17,1],[28,28,1],[77,117,3],[76,109,4],[61,61,1],[84,92,5],[18,41,4],[47,55,9],[74,132,6],[53,87,3],[102,131,7],[26,26,1],[66,68,3],[59,73,1],[22,30,9],[9,13,2],[31,35,2],[90,91,2],[72,72,1],[62,84,8],[105,106,2],[3,3,1],[32,32,1],[99,103,4],[45,52,4],[108,116,3],[91,123,8],[89,114,4],[94,130,7],[103,104,2],[14,17,4],[63,66,4],[98,112,7],[101,140,9],[58,58,1],[109,145,1],[8,15,8],[4,16,5],[115,141,1],[40,50,4],[118,118,1],[81,120,7]]";
+        String str = "[[1,3,2],[2,5,3],[5,6,2]]";
         str = str.replaceAll("\\[", "{");
         str = str.replaceAll("]", "}");
         System.out.println(str);
@@ -83,7 +58,7 @@ public class P2589 {
 
     @Test
     public void test() {
-        int[][] tasks = {{68, 129, 2}, {58, 86, 9}, {112, 142, 10}, {106, 108, 1}, {48, 48, 1}, {116, 143, 2}, {28, 43, 5}, {1, 1, 1}, {75, 83, 3}, {104, 136, 10}, {11, 11, 1}, {60, 63, 1}, {73, 111, 8}, {57, 57, 1}, {117, 119, 3}, {25, 38, 2}, {20, 21, 1}, {78, 80, 2}, {17, 17, 1}, {28, 28, 1}, {77, 117, 3}, {76, 109, 4}, {61, 61, 1}, {84, 92, 5}, {18, 41, 4}, {47, 55, 9}, {74, 132, 6}, {53, 87, 3}, {102, 131, 7}, {26, 26, 1}, {66, 68, 3}, {59, 73, 1}, {22, 30, 9}, {9, 13, 2}, {31, 35, 2}, {90, 91, 2}, {72, 72, 1}, {62, 84, 8}, {105, 106, 2}, {3, 3, 1}, {32, 32, 1}, {99, 103, 4}, {45, 52, 4}, {108, 116, 3}, {91, 123, 8}, {89, 114, 4}, {94, 130, 7}, {103, 104, 2}, {14, 17, 4}, {63, 66, 4}, {98, 112, 7}, {101, 140, 9}, {58, 58, 1}, {109, 145, 1}, {8, 15, 8}, {4, 16, 5}, {115, 141, 1}, {40, 50, 4}, {118, 118, 1}, {81, 120, 7}};
+        int[][] tasks = {{1, 3, 2}, {5, 6, 2}, {2, 5, 3}};
         int time = findMinimumTime(tasks);
         System.out.println("time = " + time);
     }
