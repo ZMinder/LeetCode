55da2276f04da58119965a7f0dd02b9c16f769e4
CMinder <minderctrl@gmail.com>
2024-05-20 19:34:45 +0800

    P40 回溯-组合数 给定的数组中存在重复的元素，每个元素最多使用一次，不允许出现重复的组合 组合数可以想象成树形结构，每一层代表组合数的第i个数，下一层只能从i之后的数选取（避免了重复的组合，要求给定数组中元素不重复） 将数组排序，重复的元素同层只选取第一个，后续相同的数字同层不再选取，但下一层可能会选取与本层相同的数（也就是同层不重复，树枝可以重复）

--- src/P1_100/P31_40/P40.java
+++ src/P1_100/P31_40/P40.java
@@ -2,64 +2,42 @@ package P1_100.P31_40;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class P40 {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        HashMap<Integer, Integer> countMap = new HashMap<>();
-        for (int i = 0; i < candidates.length; i++) {//记录candidates中每个数出现的次数
-            Integer init = countMap.getOrDefault(candidates[i], 0);
-            init++;
-            countMap.put(candidates[i], init);
-        }
-        int[] arr = new int[countMap.size()];
-        int[] size = new int[countMap.size()];
-        int index = 0;
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            arr[index] = entry.getKey();
-            size[index++] = entry.getValue();
-        }
-        backtrace(res, new ArrayList<>(), arr, size, target, 0);
+        Arrays.sort(candidates);//升序排列
+        backtrace(res, new ArrayList<>(), candidates, target, 0);
         return res;
     }
 
     /**
-     * 利用candidates数组i及其之后数，凑出target，每个数只能用一次
-     *
-     * @param res    存储所有组合
-     * @param temp   存储当前的已经选取的数字
-     * @param arr    记录提供所有提供的数据 不重复
-     * @param size   记录每个数据出现的次数
-     * @param target 要凑出的目标
-     * @param i      candidates数组下标
+     * 一层一层的来选，第一层是组合的第一个数，第二层是组合的第二个数
      */
-    public void backtrace(List<List<Integer>> res, List<Integer> temp, int[] arr,
-                          int[] size, int target, int i) {
-        if (target == 0) {//当凑够target 不再继续选取 因为后续不会凑出0
+    public void backtrace(List<List<Integer>> res, List<Integer> temp, int[] candidates,
+                          int target, int begin) {
+        if (target == 0) {
             res.add(new ArrayList<>(temp));
             return;
         }
-        if (i == arr.length) {//防止下标越界
-            return;
-        }
-        for (int j = 0; j <= size[i] && arr[i] * j <= target; j++) {//每个数可以选取多次
-            for (int k = 0; k < j; k++) {//选取j个数
-                temp.add(arr[i]);
+        for (int i = begin; i < candidates.length; i++) {
+            if (target < candidates[i]) {//如果当前数比target大
+                // 由于升序排列 后续数也一定会大于target
+                return;
             }
-            backtrace(res, temp, arr, size, target - j * arr[i], i + 1);//选取下一个数字
-            for (int k = 0; k < j; k++) {//移除
-                temp.remove(temp.size() - 1);
+            if (i > begin && candidates[i] == candidates[i - 1]) {//同一层不重复选
+                continue;
             }
+            temp.add(candidates[i]);//本层选取该数
+            backtrace(res, temp, candidates, target - candidates[i], i + 1);
+            temp.remove(temp.size() - 1);//本层移除当前数字
         }
     }
 
     @Test
     public void testConvert() {
-        String str = "[2,5,2,1,2]";
+        String str = "[10,1,2,7,6,1,5]";
         str = str.replace('[', '{');
         str = str.replace(']', '}');
         System.out.println(str);
@@ -67,8 +45,8 @@ public class P40 {
 
     @Test
     public void test() {
-        int[] candidates = {2, 5, 2, 1, 2};
-        int target = 5;
+        int[] candidates = {10, 1, 2, 7, 6, 1, 5};
+        int target = 8;
         List<List<Integer>> lists = combinationSum2(candidates, target);
         System.out.println(lists);
     }
