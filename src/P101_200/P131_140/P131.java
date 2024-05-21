67df39c3cc7bd4cc14a23a026e019a9843c98929
CMinder <minderctrl@gmail.com>
2024-05-21 22:30:31 +0800

    P131 直接考虑分割方案的首个字符串，确定了首个字符串，递归去确定下一个字符串，知道没有字符可选

--- src/P101_200/P131_140/P131.java
+++ src/P101_200/P131_140/P131.java
@@ -13,31 +13,23 @@ public class P131 {
     List<String> temp = new ArrayList<>();//存储当前分割方案
 
     public List<List<String>> partition(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            backtrace(s, i);
-        }
+        backtrace(s, 0);
         return res;
     }
 
-    /**
-     * 当前字符串str可以分割成cuts + 1个子串，cuts相当于分割的次数，在当前状态下，求解分割方案
-     */
-    public void backtrace(String str, int cuts) {
-        if (cuts == 0) {//说明已经分割完成
-            //检查最后一个子串是否是回文子串
-            if (check(str)) {
-                temp.add(str);//添加进当前分割方案
-                res.add(new ArrayList<>(temp));//添加进分割方案记录集合
-                temp.remove(temp.size() - 1);//移除当前分割方案
-            }
+    //考虑前几个字符能够构成分割方案的第一个字符串，如果可以，产生一个分割方案
+    //同时在后续字符串以同样的方式考虑后续分割方案
+    public void backtrace(String str, int begin) {
+        if (begin == str.length()) {//说明分割结束，产生了一个分割方案
+            res.add(new ArrayList<>(temp));
             return;
         }
-        for (int i = 0; i < str.length() - cuts; i++) {//至少剩余的子串 每个字符可以单独分割成一个子串
-            String sub = str.substring(0, i + 1);//当前分割的第一个子串
-            if (check(sub)) {//第一个子串是回文子串，继续分割
-                temp.add(sub);//添加进当前分割方案
-                backtrace(str.substring(i + 1), cuts - 1);
-                temp.remove(temp.size() - 1);//移除当前分割子串
+        for (int i = begin; i < str.length(); i++) {//考虑前i+1个字符能否构成回文子串
+            String sub = str.substring(begin, i + 1);
+            if (check(sub)) {//如果前i+1个字符构成回文字符串
+                temp.add(sub);
+                backtrace(str, i + 1);
+                temp.remove(temp.size() - 1);
             }
         }
     }
@@ -58,7 +50,7 @@ public class P131 {
 
     @Test
     public void test() {
-        String str = "a";
+        String str = "cbbbcc";
         List<List<String>> partition = partition(str);
         System.out.println(partition);
     }
