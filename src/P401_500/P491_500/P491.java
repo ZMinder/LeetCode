a1cf1b3da446db6f0e99dc17f59c23f0bb3b7c34
CMinder <minderctrl@gmail.com>
2024-05-24 11:04:28 +0800

    P491 每个位置的数都可能选取（存在条件：大于等于子序列最后一个数） 如果给定数组不出现相邻两个重复的数，只需要考虑不选取和满足条件的情况下选取即可，最后在begin==nums.length时结算即可 但是如果出现相邻两个重复的数，子序列就可能重复，原因在于两个数只选取一个时，两个都可以被选取，因此，设定当前数与pre相等时，跳过不选取的时候，返回上一层，在前一个数不选取时，选取后一个数，可以去除重复子序列

--- src/P401_500/P491_500/P491.java
+++ src/P401_500/P491_500/P491.java
@@ -3,7 +3,7 @@ package P401_500.P491_500;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -22,30 +22,32 @@ public class P491 {
     }
 
     /**
-     * 将子序列想象为一棵树，第i层是子序列的第i个数，同层的数不能重复，同一树枝的数可以重复
-     * 第i层的数选取范围在begin~nums.length-1，且必须大于等于pre，且同层选择的数不可重复
-     * 假设本层选取下标为j的数，i+1层选取范围要在j+1~nums.length-1内
-     * 如何确保本层选取的数不重复，使用HashSet记录已经选取的数，如果不在HashSet内代表可以选取
+     * 每个位置的数都可能被选取，选取的要求是大于等于pre
      */
     public void backtrace(int[] nums, int begin, int pre) {
-        HashSet<Integer> used = new HashSet<>();//记录当前子序列位置的可选值是否使用过
-        for (int i = begin; i < nums.length; i++) {//当前子序列某个位置可选值
-            if (nums[i] >= pre && !used.contains(nums[i])) {
-                //如果大于等于子序列的前一个数并且该数在子序列的当前位置未使用过
-                temp.add(nums[i]);//当前数加入递增子序列
-                used.add(nums[i]);//标记为使用过
-                if (temp.size() > 1) {//只要当前递增子序列至少存在两个元素 就加入结果集
-                    res.add(new ArrayList<>(temp));
-                }
-                backtrace(nums, i + 1, nums[i]);//确定数组下一个数能否进入子序列中
-                temp.remove(temp.size() - 1);//当前数不加入子序列
+        if (begin == nums.length) {//防止下标越界
+            if (temp.size() > 1) {//只要当前递增子序列至少存在两个元素 就加入结果集
+                res.add(new ArrayList<>(temp));
             }
+            return;
+        }
+        if (nums[begin] >= pre) {//大于等于前一个数
+            temp.add(nums[begin]);//当前数加入递增子序列
+            backtrace(nums, begin + 1, nums[begin]);//确定数组下一个数能否进入子序列中
+            temp.remove(temp.size() - 1);//当前数不加入子序列
+        }
+        if (nums[begin] != pre) {//只有当相邻两个数相等时，才可能出现递增子序列重复问题
+            //此时存在三种情况：1.两个数同时选取
+            //2.两个数都不选取
+            //3.只选取一个（这种情况会出现重复）
+            // nums[begin] != pre 使得只选取后一个
+            backtrace(nums, begin + 1, pre);//递增子序列不考虑当前数
         }
     }
 
     @Test
     public void test() {
-        int[] nums = {4, 4, 3, 2, 1};
+        int[] nums = {4, 6, 7, 7};
         List<List<Integer>> subsequences = findSubsequences(nums);
         System.out.println("subsequences = " + subsequences);
     }
